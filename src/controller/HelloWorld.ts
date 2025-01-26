c69ba7de359685090581627eb34d7c097ff4e513
cfdxkk <b2567240058@gmail.com>
2025-01-26 13:04:54 +0800

    ✨ test github action

--- src/controller/HelloWorld.ts
+++ src/controller/HelloWorld.ts
@@ -3,5 +3,6 @@ import { koaCtx, koaNext } from '../type/koaTypes.js'
 export const helloWorld = async (ctx: koaCtx, next: koaNext): Promise<void> => {
 	const something = ctx.query.something
 	ctx.body = something === 'Beautiful' ? `Hello Beautiful World` : 'Hello World'
+	console.log('test', process.env.test)
 	await next()
 }
